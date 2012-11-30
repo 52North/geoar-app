1ccb441a55b3dbd1a6cbed773ffc26a1a2dee937
hopmann <h.hopmann@uni-muenster.de>
2012-11-30 08:54:57 +0000

    first approach for plugin version management. plugin names and versions are generated from their filenames.

--- src/main/java/org/n52/android/newdata/DataSourceLoader.java
+++ src/main/java/org/n52/android/newdata/DataSourceLoader.java
@@ -20,9 +20,12 @@ import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.n52.android.GeoARApplication;
 import org.n52.android.newdata.CheckList.OnCheckedChangedListener;
@@ -203,8 +206,54 @@ public class DataSourceLoader {
 		mSelectedDataSourcesChangeListeners.remove(listener);
 	}
 
+	/**
+	 * Parses a version string to long. Assumes that each part of a version
+	 * string is < 100
+	 * 
+	 * @param version
+	 *            Version string, e.g. "1.2.3"
+	 * @return long built by multiplying each version component by 100 to the
+	 *         power of its position from the back, i.e. "0.0.1" -> 1, "0.1.0"
+	 *         -> 100
+	 */
+	private static long parseVersionNumber(String version) {
+		String[] split = version.split("\\.");
+		long versionNumber = 0;
+		for (int i = 0; i < split.length; i++) {
+			int num = Integer.parseInt(split[i]);
+			if (num < 0 || num >= 100) {
+				throw new NumberFormatException(
+						"Unable to parse version number, each part may not exceed 100");
+			}
+			versionNumber += Math.pow(100, (split.length - 1) - i) * num;
+		}
+		return versionNumber;
+	}
+
+	/**
+	 * Loads all plugins from SD card, compares version strings for plugins with
+	 * same names and loads the most recent plugin.
+	 * 
+	 * The plugin name is the filename without its ending and without optional
+	 * version string. A version string is introduced by a hyphen, following
+	 * dot-separated numbers, e.g. "-1.2.3"
+	 */
 	@SuppressLint("NewApi")
 	private static void loadPlugins() {
+
+		/**
+		 * Class to hold plugin version and filename information to find the
+		 * newest
+		 */
+		class PluginInfo {
+			public PluginInfo(String pluginFileName, long version) {
+				this.pluginFileName = pluginFileName;
+				this.version = version;
+			}
+
+			public String pluginFileName;
+			public long version;
+		}
 
 		String[] apksInDirectory = new File(PLUGIN_PATH)
 				.list(pluginFilenameFilter);
@@ -212,63 +261,130 @@ public class DataSourceLoader {
 		if (apksInDirectory == null || apksInDirectory.length == 0)
 			return;
 
-		try {
-			for (String pluginFileName : apksInDirectory) {
-				String pluginBaseFileName = pluginFileName.substring(0,
-						pluginFileName.lastIndexOf("."));
-				String pluginPath = PLUGIN_PATH + pluginFileName;
-				// Check if the Plugin exists
-				if (!new File(pluginPath).exists())
-					throw new FileNotFoundException("Directory not found: "
-							+ pluginPath);
+		// Pattern captures the plugin version string,
+		Pattern pluginVersionPattern = Pattern
+				.compile("-(\\d+(?:\\.\\d+)*)[.-]");
 
-				File tmpDir = GeoARApplication.applicationContext.getDir(
-						pluginBaseFileName, 0);
+		// Pattern captures the plugin name, ignoring the optional version
+		Pattern pluginNamePattern = Pattern
+				.compile("^((?:.(?!-\\d+\\.))+.).*\\.[^.]+$");
 
-				Enumeration<String> entries = DexFile.loadDex(
-						pluginPath,
-						tmpDir.getAbsolutePath() + "/" + pluginBaseFileName
-								+ ".dex", 0).entries();
-				// Path for optimized dex equals path which will be used by
-				// dexClassLoader
+		// Map to store all plugins with their versions for loading only the
+		// newest
+		HashMap<String, PluginInfo> pluginVersionMap = new HashMap<String, PluginInfo>();
 
-				// create separate ClassLoader for each plugin
-				DexClassLoader dexClassLoader = new DexClassLoader(pluginPath,
-						tmpDir.getAbsolutePath(), null,
-						GeoARApplication.applicationContext.getClassLoader());
-
-				while (entries.hasMoreElements()) {
-					// Check each classname for annotations
-					String entry = entries.nextElement();
-
-					Class<?> entryClass = dexClassLoader.loadClass(entry);
-					if (entryClass
-							.isAnnotationPresent(Annotations.DataSource.class)) {
-						// Class is a annotated as datasource
-						if (org.n52.android.newdata.DataSource.class
-								.isAssignableFrom(entryClass)) {
-							// Class is a datasource
-							@SuppressWarnings("unchecked")
-							DataSourceHolder dataSourceHolder = new DataSourceHolder(
-									(Class<? extends DataSource<? super Filter>>) entryClass);
-							mInstalledDataSources.add(dataSourceHolder);
-						} else {
-							Log.e("GeoAR",
-									"Datasource "
-											+ entryClass.getSimpleName()
-											+ " is not implementing DataSource interface");
-							// TODO handle error
-						}
-					}
-				}
-
+		Matcher matcher;
+		for (String pluginFileName : apksInDirectory) {
+			matcher = pluginNamePattern.matcher(pluginFileName);
+			if (!matcher.matches()) {
+				Log.e("GeoAR", "Plugin filename invalid: " + pluginFileName);
+				continue;
 			}
-			// TODO Handle exceptions
+
+			String pluginName = matcher.group(1);
+			if (pluginName == null) {
+				// invalid plugin filename
+				Log.e("GeoAR", "Plugin filename invalid: " + pluginFileName);
+				continue;
+			}
+
+			long version = 0;
+			matcher = pluginVersionPattern.matcher(pluginFileName);
+			if (matcher.find() && matcher.group(1) != null) {
+				try {
+					version = parseVersionNumber(matcher.group(1));
+				} catch (NumberFormatException e) {
+					Log.e("GeoAR", "Plugin filename version invalid: "
+							+ matcher.group(1));
+				}
+			}
+			PluginInfo pluginInfo = pluginVersionMap.get(pluginName);
+			if (pluginInfo == null) {
+				// Plugin not yet known
+				pluginVersionMap.put(pluginName, new PluginInfo(pluginFileName,
+						version));
+			} else {
+				// Plugin already found
+				if (pluginInfo.version < version) {
+					// current plugin has a newer version
+					pluginInfo.version = version;
+					pluginInfo.pluginFileName = pluginFileName;
+				}
+			}
+		}
+
+		try {
+			for (PluginInfo pluginInfo : pluginVersionMap.values()) {
+				loadPlugin(pluginInfo.pluginFileName);
+			}
 		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
+			// TODO Handle exceptions
 			e.printStackTrace();
 		}
 
+	}
+
+	/**
+	 * Loads a single plugin using {@link DexClassLoader}. Every annotated and
+	 * valid datasource will be added to
+	 * {@link DataSourceLoader#mInstalledDataSources};
+	 * 
+	 * @param pluginFileName
+	 * @throws IOException
+	 */
+	@SuppressLint("NewApi")
+	private static void loadPlugin(String pluginFileName) throws IOException {
+		String pluginBaseFileName = pluginFileName.substring(0,
+				pluginFileName.lastIndexOf("."));
+		String pluginPath = PLUGIN_PATH + pluginFileName;
+		// Check if the Plugin exists
+		if (!new File(pluginPath).exists())
+			throw new FileNotFoundException("Directory not found: "
+					+ pluginPath);
+
+		File tmpDir = GeoARApplication.applicationContext.getDir(
+				pluginBaseFileName, 0);
+
+		Enumeration<String> entries = DexFile
+				.loadDex(
+						pluginPath,
+						tmpDir.getAbsolutePath() + "/" + pluginBaseFileName
+								+ ".dex", 0).entries();
+
+		// Path for optimized dex equals path which will be used by
+		// dexClassLoader
+		// create separate ClassLoader for each plugin
+		DexClassLoader dexClassLoader = new DexClassLoader(pluginPath,
+				tmpDir.getAbsolutePath(), null,
+				GeoARApplication.applicationContext.getClassLoader());
+
+		try {
+			while (entries.hasMoreElements()) {
+				// Check each classname for annotations
+				String entry = entries.nextElement();
+
+				Class<?> entryClass = dexClassLoader.loadClass(entry);
+				if (entryClass
+						.isAnnotationPresent(Annotations.DataSource.class)) {
+					// Class is a annotated as datasource
+					if (org.n52.android.newdata.DataSource.class
+							.isAssignableFrom(entryClass)) {
+						// Class is a datasource
+						@SuppressWarnings("unchecked")
+						DataSourceHolder dataSourceHolder = new DataSourceHolder(
+								(Class<? extends DataSource<? super Filter>>) entryClass);
+						mInstalledDataSources.add(dataSourceHolder);
+					} else {
+						Log.e("GeoAR",
+								"Datasource "
+										+ entryClass.getSimpleName()
+										+ " is not implementing DataSource interface");
+						// TODO handle error
+					}
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Dex changed");
+		}
 	}
 }
